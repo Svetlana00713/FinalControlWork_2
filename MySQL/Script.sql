e38dc15e88893cb5e3fa85a8894a191946496f06
Svetlana00713 <123585447+Svetlana00713@users.noreply.github.com>
2024-08-08 13:02:13 +0300

    Changed Script.sql

--- MySQL/Script.sql
+++ MySQL/Script.sql
@@ -1,5 +1,4 @@
--- 7. В подключенном MySQL репозитории создать базу данных “Друзья человека”
-
+-- 7. В подключенном MySQL-репозитории создать базу данных “Друзья человека”.
 
 DROP DATABASE IF EXISTS `human_friends`;
 
@@ -7,15 +6,12 @@ CREATE DATABASE IF NOT EXISTS `human_friends`;
 
 USE `human_friends`;
 
-
--- 8. Создать таблицы с иерархией из диаграммы в БД
-
+-- 8. Создать таблицы с иерархией из диаграммы в БД.
 
 CREATE TABLE `animals` (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
   animals_class VARCHAR(30)
 );
-
 
 CREATE TABLE `dogs` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -26,7 +22,6 @@ CREATE TABLE `dogs` (
   FOREIGN KEY (`animal_class_id`) REFERENCES `animals` (`id`) ON DELETE CASCADE
 );
 
-
 CREATE TABLE `cats` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
   `name` VARCHAR(30) NOT NULL,
@@ -35,7 +30,6 @@ CREATE TABLE `cats` (
   `animal_class_id` INT UNSIGNED NOT NULL,
   FOREIGN KEY (`animal_class_id`) REFERENCES `animals` (`id`) ON DELETE CASCADE
 );
-
 
 CREATE TABLE `hamsters` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -46,7 +40,6 @@ CREATE TABLE `hamsters` (
   FOREIGN KEY (`animal_class_id`) REFERENCES `animals` (`id`) ON DELETE CASCADE
 );
 
-
 CREATE TABLE `horses` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
   `name` VARCHAR(30) NOT NULL,
@@ -55,7 +48,6 @@ CREATE TABLE `horses` (
   `animal_class_id` INT UNSIGNED NOT NULL,
   FOREIGN KEY (`animal_class_id`) REFERENCES `animals` (`id`) ON DELETE CASCADE
 );
-
 
 CREATE TABLE `camels` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -66,7 +58,6 @@ CREATE TABLE `camels` (
   FOREIGN KEY (`animal_class_id`) REFERENCES `animals` (`id`) ON DELETE CASCADE
 );
 
-
 CREATE TABLE `donkeys` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
   `name` VARCHAR(30) NOT NULL,
@@ -76,58 +67,45 @@ CREATE TABLE `donkeys` (
   FOREIGN KEY (`animal_class_id`) REFERENCES `animals` (`id`) ON DELETE CASCADE
 );
 
-
--- 9. Заполнить низкоуровневые таблицы именами(животных), командами которые они выполняют и датами рождения
-
+-- 9. Заполнить низкоуровневые таблицы именами животных, командами, которые они выполняют, и датами рождения.
 
 INSERT INTO `human_friends`.`animals` (`id`, `animals_class`) VALUES 
   ('1', 'pets'),
   ('2', 'pack_animals');
 
-
 INSERT INTO `human_friends`.`dogs` (`name`, `skills`, `birth_date`, `animal_class_id`) VALUES
-  ('Buddy', 'Sit, Stay, Fetch', '2019-02-10', 1),
-  ('Max', 'Roll over, Shake hands', '2018-06-15', 1),
-  ('Luna', 'Bark, Spin', '2016-12-08', 1);
+  ('South', 'Sit, Stay, Fetch', '2022-09-12', 1),
+  ('Nord', 'Lie down, Voice', '2018-06-15', 1),
+  ('Fram', 'Guard', '2019-08-11', 1);
  
-
 INSERT INTO `human_friends`.`cats` (`name`, `skills`, `birth_date`, `animal_class_id`) VALUES
-  ('Oliver', 'Purr, Pounce', '2018-04-25', 1),
-  ('Leo', 'Groom, Sleep', '2019-07-10', 1),
-  ('Simba', 'Meow, Play', '2016-09-12', 1);
-
+  ('Barsik', 'Hide, Food', '2020-04-25', 1),
+  ('Chubchik', 'Quiet, Sleep', '2023-07-19', 1),
+  ('Asya', 'Voice, Play', '2019-09-12', 1);
 
 INSERT INTO `human_friends`.`hamsters` (`name`, `skills`, `birth_date`, `animal_class_id`) VALUES
-  ('Coco', 'Run on the wheel', '2021-01-03', 1),
-  ('Peanut', 'Hide food, Burrow', '2022-02-14', 1),
-  ('Hazel', 'Gnaw, Explore', '2023-04-01', 1);
-
+  ('Glutton', 'Run', '2021-06-23', 1),
+  ('Fidget', 'Play, Quiet', '2022-09-04', 1),
+  ('Ginger', 'Freese', '2023-04-21', 1);
 
 INSERT INTO `human_friends`.`horses` (`name`, `skills`, `birth_date`, `animal_class_id`) VALUES
-  ('Spirit', 'Gallop, Jump', '2015-08-20', 2),
-  ('Daisy', 'Trot, Lunge', '2016-06-10', 2),
-  ('Rocky', 'Western riding, Reining', '2019-09-05', 2);
-
+  ('Florencia', 'Gallop, Jump', '2022-06-26', 2),
+  ('Wind', 'Calm down', '2020-04-30', 2);
 
 INSERT INTO `human_friends`.`camels` (`name`, `skills`, `birth_date`, `animal_class_id`) VALUES
-  ('Sahara', 'Carry loads, Long trek', '2014-12-01', 2),
-  ('Amir', 'Desert navigation, Endurance', '2015-10-18', 2),
-  ('Jamal', 'Hump storage, Adaptation', '2018-02-15', 2);
-
+  ('Zhirik', 'Carry loads, Lie', '2019-12-11', 2),
+  ('Colonel', 'Stop it, Forward', '2020-03-16', 2),
+  ('Camel', 'Take it home', '2019-06-25', 2);
 
 INSERT INTO `human_friends`.`donkeys` (`name`, `skills`, `birth_date`, `animal_class_id`) VALUES
-  ('Jack', 'Carry load, Braying', '2019-08-12', 2),
-  ('Jenny', 'Grazing, Guarding', '2020-05-05', 2),
-  ('Rosie', 'Trick training, Therapy', '2023-06-05', 2);
-  
- 
--- 10. Удалив из таблицы верблюдов, т.к. верблюдов решили перевезти в другой питомник на зимовку. 
--- Объединить таблицы лошади, и ослы в одну таблицу.
- 
+  ('Patriot', 'Forward', '2019-05-11', 2),
+  ('Simp', 'Faster', '2021-06-03', 2);
+
+-- 10. Удалить из таблицы верблюдов, т.к. верблюдов решили перевезти в другой питомник на зимовку. 
+-- Объединить таблицы "Лошади" и "Ослы" в одну таблицу.
  
 DROP TABLE `human_friends`.`camels`;
  
-
 CREATE TABLE `horses_and_donkeys` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
   `name` VARCHAR(30) NOT NULL,
@@ -138,20 +116,16 @@ CREATE TABLE `horses_and_donkeys` (
   PRIMARY KEY (`id`)
 );
 
-
 INSERT INTO `horses_and_donkeys` (`name`, `skills`, `birth_date`, `animal_class_id`, `species`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'Horse' AS `species`
 FROM `horses`;
-
 
 INSERT INTO `horses_and_donkeys` (`name`, `skills`, `birth_date`, `animal_class_id`, `species`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'Donkey' AS `species`
 FROM `donkeys`;
 
-
--- 11.Создать новую таблицу “молодые животные” в которую попадут все животные старше 1 года, но младше 3 лет 
--- и в отдельном столбце с точностью до месяца подсчитать возраст животных в новой таблице
-
+-- 11.Создать новую таблицу “молодые животные”, в которую попадут все животные старше 1 года, но младше 3 лет, 
+-- и в отдельном столбце с точностью до месяца подсчитать возраст животных в новой таблице.
 
 CREATE TABLE `young_animals` (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -160,39 +134,32 @@ CREATE TABLE `young_animals` (
   `age_months` INT NOT NULL
 );
 
-
 INSERT INTO `young_animals` (`name`, `species`, `age_months`)
 SELECT `name`, 'Dog' AS `species`, TIMESTAMPDIFF(MONTH, `birth_date`, CURDATE()) AS `age_months`
 FROM `dogs`
 WHERE `birth_date` <= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND `birth_date` >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR);
-
 
 INSERT INTO `young_animals` (`name`, `species`, `age_months`)
 SELECT `name`, 'Cat' AS `species`, TIMESTAMPDIFF(MONTH, `birth_date`, CURDATE()) AS `age_months`
 FROM `cats`
 WHERE `birth_date` <= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND `birth_date` >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR);
 
-
 INSERT INTO `young_animals` (`name`, `species`, `age_months`)
 SELECT `name`, 'Donkey' AS `species`, TIMESTAMPDIFF(MONTH, `birth_date`, CURDATE()) AS `age_months`
 FROM `donkeys`
 WHERE `birth_date` <= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND `birth_date` >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR);
-
 
 INSERT INTO `young_animals` (`name`, `species`, `age_months`)
 SELECT `name`, 'Hamster' AS `species`, TIMESTAMPDIFF(MONTH, `birth_date`, CURDATE()) AS `age_months`
 FROM `hamsters`
 WHERE `birth_date` <= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND `birth_date` >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR);
 
-
 INSERT INTO `young_animals` (`name`, `species`, `age_months`)
 SELECT `name`, 'Horse' AS `species`, TIMESTAMPDIFF(MONTH, `birth_date`, CURDATE()) AS `age_months`
 FROM `horses`
 WHERE `birth_date` <= DATE_SUB(CURDATE(), INTERVAL 1 YEAR) AND `birth_date` >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR);
 
-
 -- 12. Объединить все таблицы в одну, при этом сохраняя поля, указывающие на прошлую принадлежность к старым таблицам.
-
 
 CREATE TABLE `all_animals` (
    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -204,26 +171,21 @@ CREATE TABLE `all_animals` (
   PRIMARY KEY (`id`)
 );
 
-
 INSERT INTO `all_animals` (`name`, `skills`, `birth_date`, `animal_class_id`, `source_table`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'dogs' AS `source_table`
 FROM `dogs`;
-
 
 INSERT INTO `all_animals` (`name`, `skills`, `birth_date`, `animal_class_id`, `source_table`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'cats' AS `source_table`
 FROM `cats`;
 
-
 INSERT INTO `all_animals` (`name`, `skills`, `birth_date`, `animal_class_id`, `source_table`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'donkeys' AS `source_table`
 FROM `donkeys`;
 
-
 INSERT INTO `all_animals` (`name`, `skills`, `birth_date`, `animal_class_id`, `source_table`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'hamsters' AS `source_table`
 FROM `hamsters`;
-
 
 INSERT INTO `all_animals` (`name`, `skills`, `birth_date`, `animal_class_id`, `source_table`)
 SELECT `name`, `skills`, `birth_date`, `animal_class_id`, 'horses' AS `source_table`
